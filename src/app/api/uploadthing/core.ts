3f4dd6f23dd32eae1d0026262837b6fa89954137
Enoch Kambale <campnoch@gmail.com>
2024-05-05 10:14:33 +0300

    feat: dashboard, file upload, file deletion, pdf rendering

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,29 @@
+import { db } from "@/db";
+import { currentUser } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  pdfUploader: f({ pdf: { maxFileSize: "8MB" } })
+    .middleware(async ({ req }) => {
+      const user = await currentUser();
+
+      if (!user || !user.id) throw new Error("Unauthorized");
+
+      return { userId: user.id };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      const createdFile = await db.file.create({
+        data: {
+          key: file.key,
+          name: file.name,
+          userId: metadata.userId,
+          url: file.url,
+          uploadStatus: "PROCESSING",
+        },
+      });
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -2,6 +2,8 @@ import { currentUser } from "@clerk/nextjs/server";
 import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
+import { z } from "zod";
+
 export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
     const user = await currentUser();
@@ -34,6 +36,46 @@ export const appRouter = router({
       },
     });
   }),
+
+  getFile: privateProcedure
+    .input(z.object({ key: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+      const file = await db.file.findFirst({
+        where: {
+          key: input.key,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      return file;
+    }),
+
+  deleteFile: privateProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      await db.file.delete({
+        where: {
+          id: input.id,
+          userId,
+        },
+      });
+
+      return file;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
