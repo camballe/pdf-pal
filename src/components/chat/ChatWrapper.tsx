6474f082b3d3942790bb447819e1f510c6c74fc9
Enoch Kambale <campnoch@gmail.com>
2024-05-06 19:54:34 +0300

    style: change color of loading spinner from red to green

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -25,7 +25,7 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
         <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
             <div className='flex-1 flex justify-center items-center flex-col mb-28'>
                 <div className='flex flex-col items-center gap-2'>
-                    <Loader2 className="h-8 w-8 text-red-500 animate-spin" />
+                    <Loader2 className="h-8 w-8 text-green-500 animate-spin" />
                     <h3 className='font-semibold text-xl'>Loading...</h3>
                     <p className='text-zinc-500 text-sm'>
                         Preparing your document
@@ -41,7 +41,7 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
         <div className='relative min-h-full bg-zinc-50 flex divide-y divide-zinc-200 flex-col justify-between gap-2'>
             <div className='flex-1 flex justify-center items-center flex-col mb-28'>
                 <div className='flex flex-col items-center gap-2'>
-                    <Loader2 className="h-8 w-8 text-red-500 animate-spin" />
+                    <Loader2 className="h-8 w-8 text-green-500 animate-spin" />
                     <h3 className='font-semibold text-xl'>Processing your document...</h3>
                     <p className='text-zinc-500 text-sm'>
                         This won&apos;t take long
